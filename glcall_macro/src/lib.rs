057e498fb5f5ce56a5f3567a60b3147a9f97b20a
Jeremy Irvine <jeremyirvine.mmprod@gmaill.com>
2023-03-21 10:04:28 -0400

    cleanup warnings

--- glcall_macro/src/lib.rs
+++ glcall_macro/src/lib.rs
@@ -5,12 +5,10 @@ use quote::{quote, ToTokens};
 
 #[proc_macro]
 pub fn gl_call(body: TokenStream) -> TokenStream {
-    let body_cloned = body.clone();
     let parsed = parse_macro_input!(body as Block);
     let stmts = parsed.stmts;
 
-
-    stmts.iter().enumerate().map(|(i, stmt)| -> TokenStream {
+    stmts.iter().map(|stmt| -> TokenStream {
         let stmt_str = stmt.to_token_stream().to_string();
         TokenStream::from(quote!(
             gl_clear_errors();
